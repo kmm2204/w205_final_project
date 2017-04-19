ab35a59230e70ac7a5fff85ff28427994bfaa4bd
kmm2204 <krista.mar@ischool.berkeley.edu>
2017-04-18 22:11:59 -0400

    update load_data_lake.sh

--- load_data_lake.sh
+++ load_data_lake.sh
@@ -8,7 +8,7 @@ cd data
 
 #strip headers
 tail -n +2 "country_codes.csv" > countrycodes.csv
-tail -n +2 "real_property_master.csv" > realproperty.csv
+tail -n +2 "real_property_master.csv" > realpropertymaster.csv
 tail -n +2 "document_control_codes.csv" > documentcontrolcodes.csv
 tail -n +2 "real_property_parties.csv" > realpropertyparties.csv
 tail -n +2 "property_type_codes.csv" > propertytypecodes.csv
@@ -20,16 +20,21 @@ tail -n +2 "ucc_collateral_codes.csv" > ucccollateralcodes.csv
 hdfs dfs -mkdir /user/w205/final-project/
 hdfs dfs -mkdir /user/w205/final-project/data
 hdfs dfs -mkdir /user/w205/final-project/data/countrycodes
+hdfs dfs -mkdir /user/w205/final-project/data/realpropertymaster
 hdfs dfs -mkdir /user/w205/final-project/data/documentcontrolcodes
 hdfs dfs -mkdir /user/w205/final-project/data/realpropertyparties
+hdfs dfs -mkdir /user/w205/final-project/data/propertytypecodes
 hdfs dfs -mkdir /user/w205/final-project/data/realpropertypartiesold
 hdfs dfs -mkdir /user/w205/final-project/data/realpropertylegals
 hdfs dfs -mkdir /user/w205/final-project/data/ucccollateralcodes
 
 #move the files to hdfs
 hdfs dfs -put /data/acris-download/data/countrycodes.csv /user/w205/final-project/data/countrycodes
+hdfs dfs -put /data/acris-download/data/realpropertymaster.csv /user/w205/final-project/data/realpropertymaster
 hdfs dfs -put /data/acris-download/data/documentcontrolcodes.csv /user/w205/final-project/data/documentcontrolcodes
 hdfs dfs -put /data/acris-download/data/realpropertyparties.csv /user/w205/final-project/data/realpropertyparties
+hdfs dfs -put /data/acris-download/data/propertytypecodes.csv /user/w205/final-project/data/propertytypecodes
 hdfs dfs -put /data/acris-download/data/realpropertypartiesold.csv /user/w205/final-project/data/realpropertypartiesold
 hdfs dfs -put /data/acris-download/data/realpropertylegals.csv /user/w205/final-project/data/realpropertylegals
 hdfs dfs -put /data/acris-download/data/ucccollateralcodes.csv /user/w205/final-project/data/ucccollateralcodes
+
