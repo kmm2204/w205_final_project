e9b32f19107591407a66758307d6e8b834750e92
Riley Rustad <rileyrustad@gmail.com>
2017-04-23 22:37:54 -0700

    edit sparkparse

--- spark_parse.py
+++ spark_parse.py
@@ -9,24 +9,28 @@ no_head_prop = prop.zipWithIndex().filter(lambda (row,index): index > 0).keys()
 not_trump = ["TRUMPET", "STRUMPFLER", "STRUMPF", "TRUMPY", "TRUMPER", "TRUMPF"]
 
 def trump_filter(x):
-	if "TRUMP" in x and x not in not_trump:
-		return True
+	if "TRUMP" in x:
+		for name in not_trump:
+			if name in x:
+				return False
+			else:
+				return True
 	else:
 		return False
 
 prop_trump = no_head_prop.filter(trump_filter)
 
-def spliter(x):
+def splitter(x):
 	tmp = x.split(",")
 	return (tmp[0],tmp[1:])
 
-prop_trump_tuples = prop_trump.map(spliter)
+prop_trump_tuples = prop_trump.map(splitter)
 
 master = sc.textFile("file:///data/acris-download/data/real_property_master.csv")
 
 no_head_master = master.zipWithIndex().filter(lambda (row,index): index > 0).keys()
 
-master_tuples = no_head_master.map(spliter)
+master_tuples = no_head_master.map(splitter)
 
 merged = prop_trump_tuples.leftOuterJoin(master_tuples)
 
@@ -35,4 +39,4 @@ def combine(x):
 
 cleaned_merge = merged.map(combine)
 
-cleaned_merge.saveAsTextFile('/data/merged_Trump_and_Master')
+cleaned_merge.saveAsTextFile('file:///data/merged_trump_and_master')
